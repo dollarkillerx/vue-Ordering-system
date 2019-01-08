017b40841561e2ae23d0f4de7027ec57b41b8d32
dollarkillerx <dollarkiller@vip.qq.com>
2019-01-08 11:32:03 +0800

    update
    update

--- src/main.js
+++ src/main.js
@@ -8,13 +8,24 @@ import About from './components/about/About'
 import Login from './components/Login'
 import Register from './components/Register'
 
+//二级路由
+import Contact from './components/about/Contact'
+import Delivery from './components/about/Delivery'
+import OrderingGuide from './components/about/OrderingGuide'
+import History from './components/about/History'
+
 Vue.use(VueRouter);
 
 const routes = [
   {path:'/',name:"homeLink",component:Home},
   {path:'/menu',name:'menuLink',component:Menu},
   {path:'/admin',component:Admin},
-  {path:'/about',component:About},
+  {path:'/about',component:About,children:[
+      {path:'/about/contact',name:'contactLink',component:Contact},
+      {path:'/about/delivery',name:'deliveryLink',component:Delivery},
+      {path:'/about/orderingGuide',name:'orderingGuideLink',component:OrderingGuide},
+      {path:'/about/history',name:'historyLink',component:History}
+    ]},
   {path:'/login',component:Login},
   {path:'/register',component:Register},
   {path:'*',redirect:'/'},
