b9b527410f9708ee70e78b2f11e30c4e3df98ecf
dollarkillerx <dollarkiller@vip.qq.com>
2019-01-08 10:11:07 +0800

    update
    update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# vue-Ordering-system
-vue Ordering system demo
+# pizza-app
+
+> A Vue.js project
+
+## Build Setup
+
+``` 
+bash
+# install dependencies
+npm install
+
+```
+
+## 基于VUE2.X的Pizza点餐系统demo
+
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-header></app-header>
+    </div>
+  </div>
+</template>
+
+<script>
+  import  AppHeader from './components/Header';
+export default {
+  name: 'app',
+  components:{
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
