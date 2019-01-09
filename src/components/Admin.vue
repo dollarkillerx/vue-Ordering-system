0f6c7efc6371b6b9556e5510ab479f9a8240ae2d
dollarkillerx <dollarkiller@vip.qq.com>
2019-01-09 10:14:05 +0800

    beforeRouteLeave离开主键之前
    离开主键之前
    beforeRouteLeave

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -10,12 +10,20 @@
             name:'Admin'
           }
         },
-      //进入主键之前
-      beforeRouteEnter:(to,from,next)=>{
-        // alert('Hello'+this.name);
-        next(vm => {
-          alert('Hello'+vm.name);//异步
-        })
+      // //进入主键之前
+      // beforeRouteEnter:(to,from,next)=>{
+      //   // alert('Hello'+this.name);
+      //   next(vm => {
+      //     alert('Hello'+vm.name);//异步
+      //   })
+      // }
+      //离开主键之前
+      beforeRouteLeave:(to,from,next)=>{
+        if (confirm('确定离开吗')){
+            next();
+        }else{
+          next(false);
+        }
       }
     }
 </script>
